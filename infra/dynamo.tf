d2effbb7d783424b244a945d8f800c2cef1b419f
lukas-camilo <lukscamilo@gmail.com>
2024-09-23 21:13:06 -0300

    Ajustando infra dynamo

---
+++ infra/dynamo.tf
@@ -0,0 +1,20 @@
+resource "aws_dynamodb_table" "products" {
+    name = "ProductsTable"
+    billing_mode = "PROVISIONED"
+    read_capacity = 10
+    write_capacity = 5
+
+    hash_key = "productId"
+    attribute {
+    name = "productIdId"
+    type = "S"  # String data type
+    }
+
+    lifecycle {
+        prevent_destroy = true
+    }
+
+    tags = {
+    Name = "ProductsTable"
+    }
+}
